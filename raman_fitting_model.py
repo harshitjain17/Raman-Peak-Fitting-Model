9b1f4b30f7793a62072ae57f0d0de17a210ad227
Harshit Jain <harshitj.cs@gmail.com>
2023-03-01 22:52:59 -0500

    so far

--- raman_fitting_model.py
+++ raman_fitting_model.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from ramanfitter import RamanFitter
 from ramanfitter.mapper import Mapper
+import matplotlib.pyplot as plt
 
 filename = os.path.join('GC_532nm.txt' ) # Get File
 data = np.genfromtxt(filename) # Open File
@@ -11,6 +12,8 @@ data = np.genfromtxt(filename) # Open File
 raman_shift = data[:, 0] # Parse x-values - typically cm^-1 or nm values
 intensity   = data[:, 1] # Parse y-values - typically intensity or counts
 
+# using zip() to create a dictionary of (raman_shift: intensity pairs)
+txt_file_dictionary = dict(zip(raman_shift, intensity))
 
 # Load the user input for the bounds of the center of the peak from an Excel file
 bounds_df = pd.read_excel('center_bounds.xlsx')
@@ -29,7 +32,7 @@ raman_fitter = RamanFitter(
         x            = raman_shift, # a 1D array of the x-axis values
         y            = intensity,   # a 1D array of the y-axis values
         autorun      = False,       # attempt to calculate all steps and fit a model
-        threshold    = 12,           # helps the code determine what is a worthy peak and what isn't
+        threshold    = 12,          # helps the code determine what is a worthy peak and what isn't
         PercentRange = 0.11,        # determines what percent error the fit model can have in regards to the amplitude and position of fit curves under found peaks
         Sigma        = 15,          # the expected width of fit curves, in terms of data points
         SigmaMin     = 3,           # the expected minimum width allowed of fit curves, in terms of data points
@@ -43,36 +46,35 @@ raman_fitter = RamanFitter(
 raman_fitter.NormalizeData()
 
 # Removes noise from input data
-# raman_fitter.Denoise(
-#         UseFFT          = True,     # a Fast Fourier Transform will be used to cutoff higher frequency noise and transformed back
-#         FFT_PS_Cutoff   = 1./1200., # this value is used to differentiate between noise and data
-#         UseSavgol       = True,     # this function will implement a Savitzky Golay filter to remove noise
-#         SavgolWindow    = 25,       # how many datapoints to iterate over in the Savitzky Golay filter
-#         SavgolOrder     = 3,        # what order of polynomial to use for the designated Savitzky Golay filter window
-#         ShowPlot        = True      # this will show a plot of the smoothed data
-#     )
+raman_fitter.Denoise(
+        UseFFT          = True,     # a Fast Fourier Transform will be used to cutoff higher frequency noise and transformed back
+        FFT_PS_Cutoff   = 1./1200., # this value is used to differentiate between noise and data
+        UseSavgol       = True,     # this function will implement a Savitzky Golay filter to remove noise
+        SavgolWindow    = 25,       # how many datapoints to iterate over in the Savitzky Golay filter
+        SavgolOrder     = 3,        # what order of polynomial to use for the designated Savitzky Golay filter window
+        ShowPlot        = True      # this will show a plot of the smoothed data
+    )
 
 # Find the peaks in the data
 raman_fitter.FindPeaks(
-        center_bounds    = center_bounds, # a dict center_bounds[peak_index] = [center_min, center_max] for bounds to find peak
-        DistBetweenPeaks = 1,  # minimum distance between peaks, in terms of data points
-        showPlot         = True # this will show a plot of the found peaks
+        txt_file_dictionary = txt_file_dictionary,  # a dictionary of (raman_shift: intensity pairs)
+        center_bounds       = center_bounds,        # a dict center_bounds[peak_index] = [center_min, center_max] for bounds to find peak
+        DistBetweenPeaks    = 1,                    # minimum distance between peaks, in terms of data points
+        showPlot            = True                  # this will show a plot of the found peaks
     )
 
 
 # Fits the data with associated curve types
 raman_fitter.FitData(
-        type     = 'Voigt', # which type of curve to use for peak - options are 'Lorentzian', 'Gaussian', and 'Voigt'
-        showPlot = True          # this will show a plot of the fit data
+        type     = 'Gaussian', # which type of curve to use for peak - options are 'Lorentzian', 'Gaussian', and 'Voigt'
+        showPlot = True     # this will show a plot of the fit data
     )
 
 
-# components  = raman_fitter.comps      # Returns a dictionary of each curve plot
+components  = raman_fitter.comps      # Returns a dictionary of each curve plot
 curveParams = raman_fitter.params     # Returns a dictionary of the parameters of each Lorentzian, Gaussian, or Voigt curve
 bestFitLine = raman_fitter.fit_line   # Returns the plot data of the model
 
-# for i in curveParams:
-#     print (f'{i} : {curveParams[i]} ' , end="\n")
 
 '''
 This will create an .xlsx file named fitted_data.xlsx in the current working directory with three columns:
