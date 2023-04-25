5d6bc8b84eed038e07966a5608c8b68f456d30c4
Harshit Jain <harshitj.cs@gmail.com>
2023-04-25 02:00:33 -0400

    removed the usage of ramanfitter; stage1 and stage2 edited

--- raman_fitting_model.py
+++ raman_fitting_model.py
@@ -1,13 +1,12 @@
 import os
 import numpy as np
 import pandas as pd
-from ramanfitter import RamanFitter
-from ramanfitter.mapper import Mapper
-import matplotlib.pyplot as plt
+# from ramanfitter import RamanFitter
+from ramanfitter_stage1 import RamanFitter
 
 filename = 'GC_532nm'
 file = os.path.join('results', filename, f'{filename}.txt') # Get File
-curves_data = np.genfromtxt(file) # Open File
+curves_data = np.genfromtxt(file)                           # Open File
 
 # Extract the Raman shift and intensity values
 raman_shift = curves_data[:, 0] # Parse x-values - typically cm^-1 or nm values
@@ -32,15 +31,18 @@ for i, row in bounds_df.iterrows():
     center_bounds[peak_index] = [center_min, center_max, type]
 
 raman_fitter = RamanFitter(
-        x            = raman_shift, # a 1D array of the x-axis values
-        y            = intensity,   # a 1D array of the y-axis values
-        autorun      = False,       # attempt to calculate all steps and fit a model
-        threshold    = 12,          # helps the code determine what is a worthy peak and what isn't
-        PercentRange = 0.11,        # determines what percent error the fit model can have in regards to the amplitude and position of fit curves under found peaks
-        Sigma        = 15,          # the expected width of fit curves, in terms of data points
-        SigmaMin     = 3,           # the expected minimum width allowed of fit curves, in terms of data points
-        SigmaMax     = 50           # the expected maximum width allowed of fit curves, in terms of data points
-    )
+        x                   = raman_shift,        # a 1D array of the x-axis values
+        y                   = intensity,          # a 1D array of the y-axis values
+        autorun             = False,              # attempt to calculate all steps and fit a model
+        threshold           = 12,                 # helps the code determine what is a worthy peak and what isn't
+        PercentRange        = 0.11,               # determines what percent error the fit model can have in regards to the amplitude and position of fit curves under found peaks
+        Sigma               = 15,                 # the expected width of fit curves, in terms of data points
+        SigmaMin            = 3,                  # the expected minimum width allowed of fit curves, in terms of data points
+        SigmaMax            = 50,                 # the expected maximum width allowed of fit curves, in terms of data points
+        txt_file_dictionary = txt_file_dictionary,# a dictionary of (raman_shift: intensity pairs)
+        center_bounds       = center_bounds,      # a dictionary version of peak_index, bounds, and type_of_curve (which type of curve to use for peaks. Options are 'Lorentzian', 'Gaussian', and 'Voigt')
+        filename            = filename            # name of the current file being examined
+)
 
 
 # Normalizes `y` data to 1
@@ -58,8 +60,6 @@ raman_fitter.NormalizeData()
 
 # Find the peaks in the data
 raman_fitter.FindPeaks(
-        txt_file_dictionary = txt_file_dictionary,  # a dictionary of (raman_shift: intensity pairs)
-        center_bounds       = center_bounds,        # a dict center_bounds[peak_index] = [center_min, center_max, type] for bounds to find peak
         DistBetweenPeaks    = 1,                    # minimum distance between peaks, in terms of data points
         showPlot            = True                  # this will show a plot of the found peaks
     )
@@ -67,16 +67,12 @@ raman_fitter.FindPeaks(
 
 # Fits the data with associated curve types
 raman_fitter.FitData(
-        txt_file_dictionary = txt_file_dictionary,  # a dictionary of (raman_shift: intensity pairs)
-        center_bounds = center_bounds,              # a dict center_bounds[peak_index] = [center_min, center_max, type] for bounds to find peak
         showPlot      = True                        # this will show a plot of the fit data
     )
-
 
 components  = raman_fitter.comps      # Returns a dictionary of each curve plot
 curveParams = raman_fitter.params     # Returns a dictionary of the parameters of each Lorentzian, Gaussian, or Voigt curve
 bestFitLine = raman_fitter.fit_line   # Returns the plot data of the model
-
 
 
 '''--------------------------------------Results--------------------------------------'''
